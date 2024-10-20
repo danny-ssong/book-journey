c19088741959bc258ad123dbbea3ac6e0d3c57f3
Song <ssb00161@naver.com>
2024-10-20 21:58:37 +0900

    update | book fetching logic from useEffect to event handler 인풋으로부터 입력받은 값을 query state 업데이트 후 book fetching을 effect를 사용했는데, 이벤트핸들러로 변경

--- src/app/posts/_components/BookSearchInput.tsx
+++ src/app/posts/_components/BookSearchInput.tsx
@@ -1,16 +1,18 @@
-import { use, useEffect, useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 import searchBooks from "@/app/actions/searchBooks";
+import { useRouter } from "next/navigation";
 
 type Props = {
-  selectedBook: any;
+  selectedBook: SearchedBook | undefined;
   onSelectBook: (book: any) => void;
-  onSearchBook?: (query: string) => void;
 };
 
-export default function BookSearchInput({ selectedBook, onSelectBook, onSearchBook = undefined }: Props) {
+export default function BookSearchInput({ selectedBook, onSelectBook }: Props) {
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [showDropDown, setShowDropDown] = useState<boolean>(false);
   const [searchedBooks, setSearchedBooks] = useState<SearchedBook[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const timeoutRef = useRef<number | null>(null);
 
   useEffect(() => {
     if (searchQuery.length < 2) {
@@ -19,9 +21,8 @@ export default function BookSearchInput({ selectedBook, onSelectBook, onSearchBo
     }
     setShowDropDown(true);
     const searchBookListHandler = setTimeout(async () => {
-      const object = await searchBooks(searchQuery, 5);
-      const books = object.documents?.map((document: SearchedBook) => document);
-
+      const response = await searchBooks(searchQuery, 5);
+      const books = response.documents;
       setSearchedBooks(books);
     }, 1000);
 
@@ -39,34 +40,46 @@ export default function BookSearchInput({ selectedBook, onSelectBook, onSearchBo
     };
   }, []);
 
-  const handleSearchBook = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!!onSearchBook) {
-      onSearchBook(searchQuery);
-    }
-  };
-
   const handleSelectBook = (book: SearchedBook) => {
-    setShowDropDown(false);
     onSelectBook(book);
   };
 
   const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value);
-    onSelectBook(null);
+    const query = e.target.value;
+    setSearchQuery(query);
+    onSelectBook(undefined);
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    if (query.length < 2) {
+      setShowDropDown(false);
+      setSearchedBooks([]);
+      return;
+    }
+
+    timeoutRef.current = window.setTimeout(async () => {
+      const response = await searchBooks(query, 5);
+      const books = response.documents;
+      setSearchedBooks(books);
+      setShowDropDown(true);
+    }, 500);
+  };
+
+  const handleInputFocus = () => {
+    setShowDropDown(true);
   };
 
   return (
     <div>
-      <form
-        onSubmit={handleSearchBook}
-        className="w-[400px] h-10 flex justify-between items-center bg-slate-200 pl-4 pr-2 rounded-full"
-      >
+      <form className="w-[400px] h-10 flex justify-between items-center bg-slate-200 pl-4 pr-2 rounded-full">
         <input
+          ref={inputRef}
           className="w-[360px] h-full"
           type="text"
           onChange={handleSearchInputChange}
           value={selectedBook ? selectedBook.title : searchQuery}
+          onFocus={handleInputFocus}
         />
         <svg
           width="24"
@@ -74,7 +87,6 @@ export default function BookSearchInput({ selectedBook, onSelectBook, onSearchBo
           focusable="false"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
-          onClick={handleSearchBook}
           className="cursor-pointer"
         >
           <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
@@ -88,6 +100,9 @@ export default function BookSearchInput({ selectedBook, onSelectBook, onSearchBo
               className="px-4 py-2 cursor-pointer flex justify-between gap-4"
               onClick={() => handleSelectBook(book)}
             >
+              <span className="w-16 h-16 truncate">
+                <img src={book.thumbnail} alt={book.title} />
+              </span>
               <span className="text-nowrap truncate max-w-[350px]">{book.title}</span>
               <span className="text-nowrap truncate max-w-[350px]">{book.authors[0]}</span>
             </li>
