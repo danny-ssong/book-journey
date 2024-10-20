7be50a6ec63e97417c3c77037361f92815234019
Song <ssb00161@naver.com>
2024-10-20 21:59:06 +0900

    add | react-query v5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import { createClient } from "@/utils/supabase/server";
 import "./globals.css";
 import Link from "next/link";
 import LogoutButton from "./_components/LogoutButton";
-import BookSearchArea from "./_components/BookSearchArea";
+import BookSearchArea from "./_components/MainBookSearchInput";
+import ReactQueryProvider from "./_components/ReactQueryProvider";
 
 export default async function RootLayout({
   children,
@@ -16,10 +17,10 @@ export default async function RootLayout({
     <html lang="en">
       <body className="w-full h-screen">
         <div className="w-full h-full flex flex-col">
-          <header className="w-full min-h-[80px] flex bg-slate-500 justify-between items-center">
+          <header className="w-full min-h-[64px] flex bg-slate-500 justify-between items-center fixed">
             <div className="flex justify-between items-center w-[800px] pl-10">
               <div>Book-Journey Logo</div>
-              <nav className="flex">
+              <nav>
                 <ul className="flex gap-10">
                   <li>
                     <Link href="/">홈</Link>
@@ -33,7 +34,7 @@ export default async function RootLayout({
             </div>
             {!!user.user ? <LogoutButton /> : <Link href="/login">로그인</Link>}
           </header>
-          <div className="px-10 pt-10 bg-red-50 flex flex-1">
+          <div className="px-10 mt-[64px] pt-10 bg-red-50 flex flex-1 ">
             <aside className="w-[200px] bg-blue-200 px-5 py-4">
               <ul>
                 <li className="mb-5 border bg-slate-800 text-white px-4 py-2 w-full text-center">
@@ -47,8 +48,9 @@ export default async function RootLayout({
                 </li>
               </ul>
             </aside>
-            <main className="w-full flex justify-center py-4">{children}</main>
-            <aside></aside>
+            <ReactQueryProvider>
+              <main className="w-full flex justify-center py-4">{children}</main>
+            </ReactQueryProvider>
           </div>
         </div>
       </body>
