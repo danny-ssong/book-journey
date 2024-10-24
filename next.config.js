51a456333e2febb8091a33edc5cbb9b4cd71799b
danny.song <ssb00161@naver.com>
2024-10-24 10:52:47 +0900

    update | images domains -> remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   images: {
-    domains: ["search1.kakaocdn.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "search1.kakaocdn.net",
+        port: "", // 필요에 따라 포트를 설정합니다.
+        pathname: "/**", // 모든 경로를 허용
+      },
+    ],
   },
 };
