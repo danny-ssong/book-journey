939a3b1d338a012707a821963de4e70d975c60b8
danny.song <ssb00161@naver.com>
2024-10-25 15:05:17 +0900

    update | text

--- src/app/manage/[user_id]/layout.tsx
+++ src/app/manage/[user_id]/layout.tsx
@@ -12,7 +12,7 @@ export default async function Layout({ children, params }: Props) {
   const supabse = createClient();
   const user = await getUserOnServer();
   if (user?.id !== params.user_id) {
-    return <>please login</>;
+    return <>잘못된 접근입니다.</>;
   }
   return <>{children}</>;
 }
