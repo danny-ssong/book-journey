56c3a0c62e78d82dd95a1de1f0cc74f5a2d92888
Song <ssb00161@naver.com>
2025-05-11 17:35:32 +0900

    fix | next build error, make new client componet and force-dynamic declaration in Page compnent

---
+++ src/app/posts/new/_components/NewPostClient.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import PostForm from "@/app/posts/_components/PostForm";
+import { searchBooks } from "@/app/books/_lib/book";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Book } from "@/types/book";
+import { useUser } from "@/app/_hooks/useUser";
+
+export default function NewPostClient() {
+  const [initBook, setInitBook] = useState<Book | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useSearchParams();
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    const isbn = params.get("isbn");
+
+    if (!user) {
+      router.push("/login");
+      return;
+    }
+
+    if (!isbn) {
+      setIsLoading(false);
+      return;
+    }
+
+    const fetchInitBook = async () => {
+      try {
+        const response = await searchBooks(isbn, 1, 1);
+        const initBook =
+          response?.documents && response?.documents?.length > 0
+            ? response.documents[0]
+            : undefined;
+        setInitBook(initBook);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchInitBook();
+  }, []);
+
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
+
+  return <PostForm initBook={initBook} />;
+}
