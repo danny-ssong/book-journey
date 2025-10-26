880b6be9698b2bd3832085c9ebcd7710a8c95105
danny-ssong <ssb00161@naver.com>
2025-10-26 22:34:33 +0900

    feat: playwright 초기세팅2 - db 초기화 로직 추가

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ dotenv.config({ path: ".env.test" });
 export default defineConfig({
   testDir: "./tests/e2e",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -16,27 +16,36 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  globalSetup: "./tests/e2e/fixture/db-reset.setup.ts",
+
   use: {
     baseURL: "http://localhost:3001",
     ignoreHTTPSErrors: true,
     locale: "ko-KR",
     timezoneId: "Asia/Seoul",
-    actionTimeout: 4000,
+    actionTimeout: 3000,
 
     trace: "on-first-retry",
   },
 
-  /* Configure projects for major browsers */
   projects: [
-    { name: "setup", testMatch: "tests/e2e/**/*.setup.spec.ts" },
+    { name: "setup", testMatch: ["tests/e2e/**/*.setup.spec.ts"] },
     {
-      name: "chromium",
+      name: "guest",
+      use: { ...devices["Desktop Chrome"] },
+      testMatch: ["tests/e2e/**/*.guest.spec.ts"],
+    },
+    {
+      name: "authenticated",
       use: {
         ...devices["Desktop Chrome"],
         storageState: "tests/e2e/.auth.json",
       },
-      testIgnore: "tests/e2e/**/*.setup.spec.ts",
-      // dependencies: ["setup"],
+      testIgnore: [
+        "tests/e2e/**/*.setup.spec.ts",
+        "tests/e2e/**/*.guest.spec.ts",
+      ],
+      dependencies: ["setup"],
     },
   ],
 
