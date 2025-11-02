b5f3063c905bdd5f351821219782c17e20c72d93
danny-ssong <ssb00161@naver.com>
2025-11-02 15:29:22 +0900

    fix: db reset, dump 파일 경로 변경

--- tests/e2e/fixture/db-reset.setup.ts
+++ tests/e2e/fixture/db-reset.setup.ts
@@ -3,8 +3,10 @@ import { execSync } from "child_process";
 export default async function resetDB() {
   console.log("resetting Database ...");
   try {
-    execSync(`cat ./tests/e2e/drop-tables.sql | docker exec -i local-postgres psql -U postgres -d bookjourney`, { stdio: "inherit" });
-    execSync(`cat ./tests/e2e/test-dump.sql | docker exec -i local-postgres psql -U postgres -d bookjourney`, { stdio: "inherit" });
+    execSync(`cat ./tests/e2e/fixture/drop-tables.sql | docker exec -i local-postgres psql -U postgres -d bookjourney`, {
+      stdio: "inherit",
+    });
+    execSync(`cat ./tests/e2e/fixture/test-dump.sql | docker exec -i local-postgres psql -U postgres -d bookjourney`, { stdio: "inherit" });
     console.log("✅ Database reset complete!");
   } catch (error) {
     console.error("❌ Failed to reset database:", error);
