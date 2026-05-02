d6d3c44e24b37e600ad35280fdb0a76eed0513b5
danny-ssong <ssb00161@naver.com>
2026-05-03 01:08:48 +0900

    chore: Next.js 설정을 TypeScript로 통합

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "search1.kakaocdn.net",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
